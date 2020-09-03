92873645dd47123951c8ec665d520ed43975b5ef
Alvaroprueba <40389952+Alvaroprueba@users.noreply.github.com>
2020-09-03 12:36:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const https = require('https')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const port = 3000
+const app = express()
+
+// serve static assets normally
+app.use(express.static(__dirname))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, '', 'index.html'))
+})
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(port, () => {
+  console.log('Listening...')
+})
 
